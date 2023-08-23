97aef7376889afe46479e59ea6ad6cbbc1055d0f
taidixiong233 <maohaoji2006@163.com>
2023-08-23 22:28:39 +0800

    修改bug

--- test/index.ts
+++
@@ -1,7 +0,0 @@
-import Error from "../src";
-import * as path from "path";
-
-const error = new Error(path.join(__dirname, "./logs"));
-
-setInterval(() => error.postError(String(Date.now())), 1000);
-setInterval(() => error.postLog(String(Date.now())), 1000);
